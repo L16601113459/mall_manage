e17c5222e1b8ec67bb56d7aca3cc0299229534c0
lwp <L16601113459@aliyun.com>
2018-03-28 23:02:14 +0800

    三月二十九号

--- src/main/java/com/atguigu/controller/IndexController.java
+++ src/main/java/com/atguigu/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+	
 	@RequestMapping("goto_sku")
 	public String goto_sku() {
 		return "sku";
@@ -16,20 +17,20 @@ public class IndexController {
 	public String goto_attr() {
 		return "attr";
 	}
-	
+
 	// spu 商品信息跳转
 	@RequestMapping("goto_spu")
 	public String goto_spu() {
 		return "spu";
 	}
-	
+
 	// 商品首页跳转
 	@RequestMapping("index")
 	public String index(String url, String title, ModelMap map) {
-		
+
 		map.put("url", url);
 		map.put("title", title);
 		return "main";
 	}
-	
+
 }
